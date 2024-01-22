8490a6bed5dffceb80fe380171993202dc204f50
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-01-22 22:50:59 +0100

    Use Container in main instead of StartScreen

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -5,28 +5,21 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 94, 4, 110),
-            Color.fromARGB(255, 44, 1, 50)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 200,
         ),
-        child: Center(
-            child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text('Learn Flutter the fun way!', style: TextStyle(color: Colors.white, fontSize: 20),),
-            TextButton(onPressed: onPressed, child: const Text('Start Quiz', style: TextStyle(color: Colors.white, fontSize: 15),))
-          ],
-        )));
+        const SizedBox(
+          height: 20,
+        ),
+        const Text('Learn Flutter the fun way!', style: TextStyle(color: Colors.white, fontSize: 20),),
+        TextButton(onPressed: onPressed, child: const Text('Start Quiz', style: TextStyle(color: Colors.white, fontSize: 15),))
+      ],
+    ));
   }
 
   void onPressed() {
