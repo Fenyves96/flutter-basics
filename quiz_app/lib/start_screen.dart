b48f04ebb19fa55516014049014efb416b0ffbf6
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-01-22 22:56:05 +0100

    formatting

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -6,20 +6,29 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 200,
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const Text('Learn Flutter the fun way!', style: TextStyle(color: Colors.white, fontSize: 20),),
-        TextButton(onPressed: onPressed, child: const Text('Start Quiz', style: TextStyle(color: Colors.white, fontSize: 15),))
-      ],
-    ));
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          TextButton(
+              onPressed: onPressed,
+              child: const Text(
+                'Start Quiz',
+                style: TextStyle(color: Colors.white, fontSize: 15),
+              ))
+        ],
+      ),
+    );
   }
 
   void onPressed() {
