6b1f8147bae6499d0bf855a277bb9e23ae5c9d59
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-02-01 21:18:32 +0100

    styling

---
+++ quiz_app/lib/answer_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  AnswerButton({super.key, required this.answerText, required this.onTap});
+
+  final void Function() onTap;
+  final String answerText;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 49, 5, 125),
+          foregroundColor: Colors.white,
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 40,
+          )),
+      child: Text(answerText),
+    );
+  }
+}
