16df8a9bb555370419ec0e540fdab57d28bd90f4
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-02-12 20:36:32 +0100

    Category enum

--- expenses/lib/model/expense.dart
+++ expenses/lib/model/expense.dart
@@ -2,11 +2,14 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();
 
   final String id;
@@ -14,4 +17,6 @@ class Expense {
   final double amount;
 
   final DateTime date;
+
+  final Category category;
 }
