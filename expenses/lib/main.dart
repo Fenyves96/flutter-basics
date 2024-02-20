486dbadf4272406e24e8d448366b481b4ccf8c43
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-02-20 22:56:37 +0100

    use common styling

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          useMaterial3: true, scaffoldBackgroundColor: Colors.lightBlue),
+      home: const Expenses(),
     ),
   );
 }
