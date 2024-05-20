5f1c8e4148ccea839110785b8a921e46a9c0143e
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-05-20 20:13:13 +0200

    new ExpenseBucket class to Store expenses by category

--- expenses/lib/models/expense.dart
+++ expenses/lib/models/expense.dart
@@ -35,3 +35,26 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpensBucket {
+  ExpensBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpensBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (Expense expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
