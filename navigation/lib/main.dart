fe61009ee48c94168e4e8b64108be838d0f38a73
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-07-03 21:29:35 +0200

    create dummy data and init CategoryGridItem

--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:navigation/cteagories.dart';
+import 'package:navigation/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
