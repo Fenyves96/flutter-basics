3a36561d8e5e84dfb89da467af23b0728f2744e7
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-08-04 23:09:22 +0200

    loading meals data into a screen

--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:navigation/data/dummy_data.dart';
 import 'package:navigation/screens/categories.dart';
+import 'package:navigation/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,7 +25,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: CategoriesScreen(),
+      home: MealsScreen(
+        title: 'Some category...',
+        meals: dummyMeals,
+      ),
     );
   }
 }
