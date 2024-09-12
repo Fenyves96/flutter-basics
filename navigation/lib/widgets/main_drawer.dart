c8cad10db5c0bf2243f60b9b260d354f1dff5344
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-09-12 22:18:54 +0200

    if current page is the same that we clicked then just close the drawer

--- navigation/lib/widgets/main_drawer.dart
+++ navigation/lib/widgets/main_drawer.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({super.key, required this.onSelectScreen});
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +47,9 @@ class MainDrawer extends StatelessWidget {
                   color: Theme.of(context).colorScheme.onBackground,
                   fontSize: 24),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen('meals');
+            },
           ),
           ListTile(
             leading: Icon(
@@ -60,7 +63,9 @@ class MainDrawer extends StatelessWidget {
                   color: Theme.of(context).colorScheme.onBackground,
                   fontSize: 24),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen('filters');
+            },
           )
         ],
       ),
