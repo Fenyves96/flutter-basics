019cc1ff1d75b77c7262b094ee41661315190001
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-11-15 22:07:46 +0100

    make up some missing consts

--- shopping_list/lib/main.dart
+++ shopping_list/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
           ),
           scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
         ),
-        home: Scaffold(
+        home: const Scaffold(
           body: GroceryList(),
         ));
   }
