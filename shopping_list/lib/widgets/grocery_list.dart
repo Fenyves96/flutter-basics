f3f9750ad0dc8b3474eae58e582b2a0cfad8cce0
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-11-24 14:36:16 +0100

    send Item to another screen + show no item if there is no item

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -10,10 +10,19 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(MaterialPageRoute(
+  final List<GroceryItem> _groceryItems = [];
+  void _addItem() async {
+    final newItem =
+        await Navigator.of(context).push<GroceryItem>(MaterialPageRoute(
       builder: (ctx) => const NewItem(),
     ));
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   @override
@@ -25,19 +34,21 @@ class _GroceryListState extends State<GroceryList> {
             IconButton(onPressed: _addItem, icon: const Icon(Icons.add))
           ],
         ),
-        body: ListView.builder(
-          itemCount: groceryItems.length,
-          itemBuilder: (ctx, index) => ListTile(
-            title: Text(groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: groceryItems[index].category.color,
-            ),
-            trailing: Text(
-              groceryItems[index].quantity.toString(),
-            ),
-          ),
-        ));
+        body: !_groceryItems.isEmpty
+            ? ListView.builder(
+                itemCount: _groceryItems.length,
+                itemBuilder: (ctx, index) => ListTile(
+                  title: Text(_groceryItems[index].name),
+                  leading: Container(
+                    width: 24,
+                    height: 24,
+                    color: _groceryItems[index].category.color,
+                  ),
+                  trailing: Text(
+                    _groceryItems[index].quantity.toString(),
+                  ),
+                ),
+              )
+            : Text('No item here'));
   }
 }
