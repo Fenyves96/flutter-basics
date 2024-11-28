6c24bd52dbd849480c7135ea2d308561b8c7c340
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-11-28 22:31:53 +0100

    delete with error handling

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 import 'package:http/http.dart' as http;
@@ -69,6 +70,21 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
+    setState(() {
+      _groceryItems.remove(item);
+    });
+    final url = Uri.https('flutte-prep-9d493-default-rtdb.firebaseio.com',
+        'shopping-list/${item.id}.json');
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(child: Text('No items added yet'));
@@ -84,10 +100,9 @@ class _GroceryListState extends State<GroceryList> {
           direction: DismissDirection.endToStart,
           key: Key(_groceryItems[index].id),
           onDismissed: (direction) {
-            var deletedItemName = _groceryItems[index].name!;
-            _groceryItems.remove(_groceryItems[index]);
-            ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text(deletedItemName + ' item deleted')));
+            _removeItem(_groceryItems[index]);
+            // ScaffoldMessenger.of(context).showSnackBar(
+            //     SnackBar(content: Text(deletedItemName + ' item deleted')));
           },
           child: ListTile(
             title: Text(_groceryItems[index].name),
